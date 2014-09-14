b9bce203c112bc13943c2eab1e24519d1d24fb19
macikokoro <betwinsouls@gmail.com>
2014-09-14 15:30:54 -0700

    adding refactor to app.js removed PanelController

--- app/js/app.js
+++ app/js/app.js
@@ -6,16 +6,6 @@
     this.product = gems;
   });
 
-  app.controller('PanelController', function(){
-    this.tab = 1;
-    this.selectTab = function(setTab) {
-      this.tab = setTab;
-    };
-    this.isSelected = function(checkTab){
-      return this.tab === checkTab;
-    };
-  });
-
   app.controller("ReviewController", function() {
     this.review = {};
 
